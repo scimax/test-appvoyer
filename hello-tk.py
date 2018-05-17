427b69e3413772e4e60166f8d3c9dfe1eef53ff6
Max Kellermeier <max.kellermeier@hotmail.de>
2018-05-17 22:55:29 +0200

    Added tests and module

--- hello-tk.py
+++ hello-tk.py
@@ -1,8 +1,13 @@
 from tkinter import *
 
-root = Tk()
+from PIL import Image
+im = Image.open("27972c514309f9787684df8c8082f53d.png")
+im.show()
 
+root = Tk()
 w = Label(root, text="Hello, world!")
 w.pack()
 
 root.mainloop()
+
+
